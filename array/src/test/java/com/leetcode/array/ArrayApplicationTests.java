24b4f42777c57817107fb31932cb5a43a9e6ea5e
姚庆奇 <yaoqngqi@bilibili.com>
2021-02-18 23:37:48 +0800

    two sum

---
+++ array/src/test/java/com/leetcode/array/ArrayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.array;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
